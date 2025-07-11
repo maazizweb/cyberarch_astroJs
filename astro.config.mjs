156c6089349c340754f4b74182bc0ecbfff5e7c8
Mohammed Abdul Aziz <ma.azizweb@gmail.com>
2025-07-11 03:07:26 -0400

    Lots of improvement all pages and logo updated

--- astro.config.mjs
+++ astro.config.mjs
@@ -3,5 +3,9 @@ import { defineConfig } from "astro/config";
 
 // https://astro.build/config
 export default defineConfig({
-  site: "https://cyberarch.ca/cybera",
+  base: "/",
+
+  devToolbar: {
+    enabled: false
+  }
 });
